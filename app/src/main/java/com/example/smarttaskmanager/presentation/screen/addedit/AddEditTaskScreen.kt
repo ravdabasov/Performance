53a635480de0c7cc66856e91d77c8bbfedc79416
ravdabasov <ravd.abasov@gmail.com>
2026-07-03 23:12:21 +0400

    Update AddEditTaskScreen.kt

--- app/src/main/java/com/example/smarttaskmanager/presentation/screen/addedit/AddEditTaskScreen.kt
+++ app/src/main/java/com/example/smarttaskmanager/presentation/screen/addedit/AddEditTaskScreen.kt
@@ -12,7 +12,6 @@ import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.matchParentSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
